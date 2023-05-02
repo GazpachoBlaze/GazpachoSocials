50fd9037655c98d08a1cfa22a9f8f197be80e7ff
Gazpacho <Gazpachoblaze@gmail.com>
2023-05-01 20:15:55 -0500

    started header and about html for web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,27 @@ import { AppRoutingModule } from './routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { SocialsComponent } from './components/socials/socials.component';
+import { StatsComponent } from './components/stats/stats.component';
+import { BottomComponent } from './components/bottom/bottom.component';
+import { HomiesComponent } from './components/homies/homies.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    SocialsComponent,
+    StatsComponent,
+    BottomComponent,
+    HomiesComponent,
+    WishlistComponent,
   ],
 
   imports: [
