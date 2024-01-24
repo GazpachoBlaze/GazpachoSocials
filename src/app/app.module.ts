ad463745078015e9e417ad720deb7047b00a5e6f
Gazpacho <Gazpachoblaze@gmail.com>
2024-01-24 16:20:04 -0600

    reworked layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
-import { Home2Component } from './components/home2/home2.component';
 import { AboutComponent } from './components/about/about.component';
 import { SocialsComponent } from './components/socials/socials.component';
 import { StatsComponent } from './components/stats/stats.component';
@@ -34,7 +33,6 @@ import { WishlistComponent } from './components/wishlist/wishlist.component';
     
     HeaderComponent,
     HomeComponent,
-    Home2Component,
     AboutComponent,
     SocialsComponent,
     StatsComponent,
